0325b55c0a10ad1544ef116e6bbdc11b12494451
Asliikl <asliikiel9@gmail.com>
2025-01-22 13:28:12 +0300

    form-merge branch'ında yeni değişiklikler

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 @section('title','The List Of Tasks')
 
 @section('content')
-        {{--@if(count($tasks))--}}
         @forelse($tasks as $task)
             <div>
             <a href="{{route('tasks.show', ['task' => $task->id])}}"> {{$task->title}}</a>
@@ -10,5 +9,4 @@
         @empty
             <div>there are no tasks!</div>
         @endforelse
-         {{--@endif--}}
 @endsection
